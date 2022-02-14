f828497362bc39b92b89604c53037dd245cce319
getchella <36796753+getchella@users.noreply.github.com>
2022-02-13 23:26:22 -0500

    Add files via upload
    GUI value blocks resized and repositioned, arrow blocks added, screen number circles added

--- NNL_Design_GUI/NNL_Design_GUI.ino
+++ NNL_Design_GUI/NNL_Design_GUI.ino
@@ -1,6 +1,7 @@
 #include "SPI.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9341.h"
+#include "stdint.h"
 
 #define WIDTH     320
 #define HEIGHT    240
@@ -17,14 +18,71 @@ void setup() {
   tft.begin();
 
   tft.setRotation(3);   // rotates the screen to horizontal
-
   tft.fillScreen(ILI9341_LIGHTGREY);   // sets background to gray
 
-  tft.drawRect(50, 50, 100, 100, ILI9341_NAVY);
+  createGuiMainFrame();
+
+  uint8_t screen = 1;
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+}
+
+/**
+ * Creates 2 main boxes that hold values and 
+ **/
+void createGuiMainFrame() {
+  tft.drawRect(30, 40, 115, 100, ILI9341_NAVY);
+  tft.drawRect(175, 40, 115, 100, ILI9341_NAVY);
+  tft.drawCircle(140, 220, 5, ILI9341_BLACK);
+  tft.drawCircle(160, 220, 5, ILI9341_BLACK);
+  tft.drawCircle(180, 220, 5, ILI9341_BLACK);
+  tft.fillCircle(140, 220, 4, ILI9341_BLACK);
+  drawRightArrowBox();
+  drawLeftArrowBox();
+}
+
+/**
+ * Creates right arrow box to move to a different screen
+ **/
+void drawRightArrowBox() {
+  tft.drawRect(245, 185, 60, 40, ILI9341_BLACK);
+  tft.fillRect(246, 186, 58, 38, ILI9341_RED);
+  tft.drawFastHLine(260, 205, 31, ILI9341_WHITE);
+  
+  int x, y_low, y_high;
+  for (x=289, y_low=204, y_high=206; x>=285 && y_low>=200 && y_high<=210; x--, y_low--, y_high++) {
+    tft.drawPixel(x, y_low, ILI9341_WHITE);
+    tft.drawPixel(x, y_high, ILI9341_WHITE);  
+  }
+}
+
+/**
+ * Creates left arrow box to move to a different screen
+ */
+void drawLeftArrowBox() {
+  tft.drawRect(15, 185, 60, 40, ILI9341_BLACK);
+  tft.fillRect(16, 186, 58, 38, ILI9341_RED);
+  tft.drawFastHLine(29, 205, 31, ILI9341_WHITE);
+
+  int x, y_low, y_high;
+  for (x=30, y_low=204, y_high=206; x<=34 && y_low>=200 && y_high<=210; x++, y_low--, y_high++) {
+    tft.drawPixel(x, y_low, ILI9341_WHITE);
+    tft.drawPixel(x, y_high, ILI9341_WHITE);
+  }
+}
+
+void setScreen1() {
+
+}
+
+void setScreen2() {
+
+}
+
+void setScreen3() {
 
 }
